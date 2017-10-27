794df281df620182a52c47f82bcea819335f2275
shuxf <shuxianfon1987@126.com>
2017-10-27 14:28:55 +0800

    提交swagger的maven依赖

---
+++ src/main/java/com/freetax/controller/TestFreetaxController.java
@@ -0,0 +1,33 @@
+package com.freetax.controller;
+
+import com.freetax.common.Response;
+import com.wordnik.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author shuxf
+ * @Date 2017/10/27 14:12
+ * 新项目测试控制器
+ */
+@RestController
+@RequestMapping("/pc/freetax/")
+public class TestFreetaxController {
+
+    /**
+     * 测试少税派接口调用
+     * @return
+     */
+    @ApiOperation(value = "测试少税派接口调用", notes = "测试少税派接口调用", response = Response.class)
+    @RequestMapping(value = "getFreeTax", method = RequestMethod.POST)
+    public Response getFreeTax(){
+        Response response = new Response();
+
+        response.setMessage("测试呀");
+        response.setCode(200);
+        response.setData("数据数据123");
+
+        return response;
+    }
+}
